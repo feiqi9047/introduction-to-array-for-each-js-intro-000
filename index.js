e14f094164f8648a77ca15cc0a3dbbf193d0528d
Fei Qi <feiqi9047@gmail.com>
2018-11-18 22:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,5 +28,7 @@ function doToElementsInArray(array, callback){
 var animals = ["dog", "cat", "squirrel"];
 
 function changeCompletely(element, index, array){
-  
+  array[index] = Math.floor(Math.random()*100+2).toString() + `${array[index]}s!!!`
 }
+
+doToElementsInArray(animals, changeCompletely)
